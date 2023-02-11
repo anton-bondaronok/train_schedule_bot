e084a3c29f47837aac6dfbdd753cf63f9ab94729
a.bondaronok <abondaronok@gmail.com>
2023-02-11 22:55:40 +0300

    rubocop fix

--- bin/bot.rb
+++ bin/bot.rb
@@ -2,11 +2,7 @@
 
 Dir['./config/initializers/*.rb'].each { |file| require file }
 
-
-
-
-
-require "telegram/bot"
+require 'telegram/bot'
 require './lib/core'
 
 Telegram::Bot::Client.run(ENV.fetch('TELEGRAM_API_TOKEN', nil)) do |bot|
